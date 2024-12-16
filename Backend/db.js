e703c6b50ebd886b6aef64ab94ccd4cba5d856bf
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2024-12-16 19:24:56 +0530

    mongoose schema of user added

--- Backend/db.js
+++ Backend/db.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://sanidhyarathore263:gmGkY2QO4weS82xW@cluster0.z6s4wp9.mongodb.net/")
+
+const userschema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String
+})
+
+module.exports = mongoose.model('User', userschema);
