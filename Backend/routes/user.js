d2e64a61b8d8739a4f1c33112f5e0e9691ab8025
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2024-12-17 21:55:00 +0530

    minor changes in signup route

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const router = express.Router();
 const zod = require("zod");
-const { jwt } = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const { User } = require("../db");
 const { JWT_SECRET } = require("../config")
 
@@ -14,15 +15,15 @@ const signupbody = zod.object({
 })
 
 router.post('/signup', async (req, res) => {
-    const data = req.body;
-    const { result } = signupbody.safeParse(data);
-    if (!result) {
+
+    const result = signupbody.safeParse(req.body);
+    if (!result.success) {
         return res.status(411).json({
             message: "Incorrect Inputs"
         });
     }
 
-    const existinguser = await User.findOne(result.email)
+    const existinguser = await User.findOne({ email: result.data.email })
     if (existinguser) {
         return res.status(411).json({
             message: "Email already taken"
@@ -30,11 +31,11 @@ router.post('/signup', async (req, res) => {
     }
 
     const user = await User.create({
-        username: result.username,
-        firstname: result.firstname,
-        lastname: result.lastname,
-        email: result.email,
-        password: result.password
+        username: result.data.username,
+        firstname: result.data.firstname,
+        lastname: result.data.lastname,
+        email: result.data.email,
+        password: result.data.password
     })
 
     const userid = user._id;
